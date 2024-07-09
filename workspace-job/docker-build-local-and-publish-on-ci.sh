2b92c60b1d43d5c6423d0acc9fc75f07a11e2db4
Oleksandr Dzhychko <odzhychko@itemis.com>
2024-07-09 09:00:28 +0200

    fix(workspace-job): use the required Java version
    Starting with MPS 2022.2 Java 17 is required.

--- workspace-job/docker-build-local-and-publish-on-ci.sh
+++ workspace-job/docker-build-local-and-publish-on-ci.sh
@@ -17,6 +17,20 @@ MODELIX_WORKSPACES_VERSION="$(cat ../workspaces-version.txt)"
 for MPS_MAJOR_VERSION in $(getProperty mpsMajorVersions | tr "," "\n"); do
   TAG=${MODELIX_WORKSPACES_VERSION}-${MPS_MAJOR_VERSION}
 
+  case $MPS_MAJOR_VERSION in
+  "2020.3" | "2021.1" | "2021.2" | "2021.3" )
+    JAVA_VERSION=11
+    ;;
+  "2022.2" | "2022.3" | "2023.2" )\
+    JAVA_VERSION=17
+    ;;
+  *)
+    echo "Unknown MPS version ${MPS_MAJOR_VERSION}. Check MPS version or extend case statement." 1>&2
+    exit 1
+  esac
+
+  echo "Build workspace-job container with MPS ${MPS_MAJOR_VERSION} and Java ${JAVA_VERSION}."
+
   (
     cd ..
     ./gradlew ":workspace-job:resolveMps${MPS_MAJOR_VERSION}"
@@ -27,10 +41,12 @@ for MPS_MAJOR_VERSION in $(getProperty mpsMajorVersions | tr "," "\n"); do
       --platform linux/amd64,linux/arm64 \
       --push \
       --build-arg MPS_MAJOR_VERSION=${MPS_MAJOR_VERSION}\
+      --build-arg JAVA_VERSION=${JAVA_VERSION}\
       -t "modelix/modelix-workspace-job:${TAG}" .
   else
     docker build \
       --build-arg MPS_MAJOR_VERSION=${MPS_MAJOR_VERSION}\
+      --build-arg JAVA_VERSION=${JAVA_VERSION}\
       -t "modelix/modelix-workspace-job:${TAG}" .
   fi
 

c13dc792d49f52f8cfdd0e8d680816364dfd8b12
Sascha Lisson <sl@q60.de>
2025-05-03 09:10:32 +0200

    feat: workspaces editor

--- workspace-manager/src/main/kotlin/org/modelix/workspace/manager/WorkspacesController.kt
+++ workspace-manager/src/main/kotlin/org/modelix/workspace/manager/WorkspacesController.kt
@@ -5,10 +5,13 @@ import io.ktor.server.application.ApplicationCall
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import org.modelix.authorization.getUserName
+import org.modelix.instancesmanager.DeploymentManager
 import org.modelix.services.workspaces.stubs.controllers.ModelixWorkspacesDraftsController
 import org.modelix.services.workspaces.stubs.controllers.ModelixWorkspacesDraftsController.Companion.modelixWorkspacesDraftsRoutes
 import org.modelix.services.workspaces.stubs.controllers.ModelixWorkspacesInstancesController
 import org.modelix.services.workspaces.stubs.controllers.ModelixWorkspacesInstancesController.Companion.modelixWorkspacesInstancesRoutes
+import org.modelix.services.workspaces.stubs.controllers.ModelixWorkspacesInstancesEnabledController
+import org.modelix.services.workspaces.stubs.controllers.ModelixWorkspacesInstancesEnabledController.Companion.modelixWorkspacesInstancesEnabledRoutes
 import org.modelix.services.workspaces.stubs.controllers.ModelixWorkspacesWorkspacesController
 import org.modelix.services.workspaces.stubs.controllers.ModelixWorkspacesWorkspacesController.Companion.modelixWorkspacesWorkspacesRoutes
 import org.modelix.services.workspaces.stubs.controllers.TypedApplicationCall
@@ -17,14 +20,18 @@ import org.modelix.services.workspaces.stubs.models.GitChangeDraftList
 import org.modelix.services.workspaces.stubs.models.GitRepository
 import org.modelix.services.workspaces.stubs.models.WorkspaceConfig
 import org.modelix.services.workspaces.stubs.models.WorkspaceInstance
+import org.modelix.services.workspaces.stubs.models.WorkspaceInstanceEnabled
 import org.modelix.services.workspaces.stubs.models.WorkspaceInstanceList
+import org.modelix.services.workspaces.stubs.models.WorkspaceInstanceState
 import org.modelix.services.workspaces.stubs.models.WorkspaceList
 import org.modelix.workspaces.DEFAULT_MPS_VERSION
+import org.modelix.workspaces.MavenRepository
 import org.modelix.workspaces.Workspace
+import java.util.UUID
 
-class WorkspacesController(val manager: WorkspaceManager) {
+class WorkspacesController(val manager: WorkspaceManager, val deployments: DeploymentManager) {
 
-    private val workspaceInstances: WorkspaceInstanceList = WorkspaceInstanceList(emptyList())
+    private var workspaceInstances: WorkspaceInstanceList = WorkspaceInstanceList(emptyList())
     private val drafts: GitChangeDraftList = GitChangeDraftList(emptyList())
 
     fun install(route: Route) {
@@ -76,6 +83,9 @@ class WorkspacesController(val manager: WorkspaceManager) {
                         gitRepositories = legacyWorkspaceConfig.gitRepositories.map {
                             org.modelix.workspaces.GitRepository(it.url, null)
                         },
+                        mavenRepositories = (legacyWorkspaceConfig.mavenRepositories ?: emptyList()).map {
+                            MavenRepository(it.url)
+                        }
                     ),
                 )
                 call.respond(HttpStatusCode.OK)
@@ -95,8 +105,47 @@ class WorkspacesController(val manager: WorkspaceManager) {
                 }
             }
 
-            override suspend fun listInstances(call: TypedApplicationCall<WorkspaceInstanceList>) {
-                call.respondTyped(workspaceInstances)
+            override suspend fun listInstances(workspaceId: String?, call: TypedApplicationCall<WorkspaceInstanceList>) {
+                val filteredInstances = if (workspaceId == null) {
+                    workspaceInstances
+                } else {
+                    workspaceInstances.copy(instances = workspaceInstances.instances.filter { it.workspaceId == workspaceId })
+                }
+                call.respondTyped(filteredInstances)
+            }
+
+            override suspend fun createInstance(
+                workspaceInstance: WorkspaceInstance,
+                call: TypedApplicationCall<WorkspaceInstance>
+            ) {
+                workspaceInstances = workspaceInstances.copy(
+                    instances = workspaceInstances.instances + workspaceInstance.copy(
+                        id = UUID.randomUUID().toString(),
+                        workspaceId = workspaceInstance.workspaceId,
+                        drafts = emptyList(),
+                        owner = call.getUserName(),
+                        state = WorkspaceInstanceState.CREATED
+                    )
+                )
+            }
+        })
+
+        modelixWorkspacesInstancesEnabledRoutes(object : ModelixWorkspacesInstancesEnabledController {
+            override suspend fun enableInstance(
+                instanceId: String,
+                workspaceInstanceEnabled: WorkspaceInstanceEnabled,
+                call: ApplicationCall
+            ) {
+                workspaceInstances = workspaceInstances.copy(
+                    instances = workspaceInstances.instances.map {
+                        if (it.id == instanceId) {
+                            it.copy(enabled = workspaceInstanceEnabled.enabled)
+                        } else {
+                            it
+                        }
+                    }
+                )
+                call.respond(HttpStatusCode.OK)
             }
         })
 
@@ -125,5 +174,6 @@ class WorkspacesController(val manager: WorkspaceManager) {
         mpsVersion = mpsVersion ?: DEFAULT_MPS_VERSION,
         memoryLimit = memoryLimit,
         gitRepositories = gitRepositories.map { GitRepository(it.url, null) },
+        mavenRepositories = mavenRepositories.map { org.modelix.services.workspaces.stubs.models.MavenRepository(it.url) },
     )
 }
